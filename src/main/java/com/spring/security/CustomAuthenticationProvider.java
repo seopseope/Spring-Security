dc8205b12e71f1a9670bb744dfd5bad50a31382c
seopseope <fbhydfa@Gmail.com>
2022-01-20 09:10:41 +0900

    Spring Security password 암호화

--- src/main/java/com/spring/security/CustomAuthenticationProvider.java
+++ src/main/java/com/spring/security/CustomAuthenticationProvider.java
@@ -6,6 +6,7 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 // 사용자가 입력한 아이디 및 비밀번호를 CustomUserDetailsService 클래스가 DB에서 가져온 사용자 정보와 비교하는 클래스이다.
 @SuppressWarnings("unused")
@@ -14,15 +15,25 @@ public class CustomAuthenticationProvider implements AuthenticationProvider{
 	@Autowired
 	private CustomUserDetailsService customUserDetailsService;
 	
+	@Autowired
+	PasswordEncoder passwordEncoder;
+	
 	@Override
 	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
 		String userId = (String)authentication.getPrincipal();
 		String userPwd = (String)authentication.getCredentials();
 		
 		CustomUserDetails user = (CustomUserDetails)customUserDetailsService.loadUserByUsername(userId);
-		System.out.println("userPwd >>> : " + userPwd);
-		System.out.println("user.getPassword >>> : " + user.getPassword());
-		if(!matchPassword(userPwd, user.getPassword())) {
+		System.out.println("사용자 입력 비밀번호 >>> : " + userPwd);
+		System.out.println("DB의 암호화된 패스워드 >>> : " + user.getPassword());
+		
+		// 암호화 하지 않은 비밀번호 비교
+//		if(!matchPassword(userPwd, user.getPassword())) {
+//			throw new BadCredentialsException(userId);
+//		}
+		
+		// BCryptPasswordEncoder로 암호화된 패스워드는 PasswordEncoder의 함수 matches로 사용자 입력값과 DB의 암호화된 패스워드 값을 비교해야한다.
+		if(!passwordEncoder.matches(userPwd, user.getPassword())) {
 			throw new BadCredentialsException(userId);
 		}
 		
@@ -34,6 +45,7 @@ public class CustomAuthenticationProvider implements AuthenticationProvider{
 		return authentication.equals(UsernamePasswordAuthenticationToken.class);
 	}
 	
+	// 비밀번호 값 비교
 	private boolean matchPassword(String userPwd, String dbPwd) {
 		return userPwd.equals(dbPwd);
 	}
